ae5e7c601163a0a10fdbe21f9b2e7aa50b29febb
Dicek <juandeee@gmail.com>
2014-10-07 13:17:29 +0000

    Done step 7

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -3,6 +3,15 @@
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
 describe('PhoneCat App', function() {
+  
+  describe('Redirect from Index', function() {
+    it('should redirect index.html to index.html#/phones', function() {
+      browser.get('app/index.html');
+      browser.getLocationAbsUrl().then(function(url) {
+          expect(url.split('#')[1]).toBe('/phones');
+        });
+    });
+  });
 
   describe('Phone list view', function() {
 
@@ -60,4 +69,19 @@ describe('PhoneCat App', function() {
      });
     
   });
+  
+  
+
+  describe('Phone detail view', function() {
+
+    beforeEach(function() {
+      browser.get('app/index.html#/phones/nexus-s');
+    });
+
+
+    it('should display placeholder page with phoneId', function() {
+      expect(element(by.binding('phoneId')).getText()).toBe('nexus-s');
+    });
+  });
+  
 });
